837767dceb32270e5f395dc881105ae8d55cfab3
HyebinPark96 <bini2718@naver.com>
2022-07-02 17:24:48 +0900

    [0702] 환경세팅 완료

---
+++ src/main/java/com/pj/cherrypick/CherrypickApplication.java
@@ -0,0 +1,13 @@
+package com.pj.cherrypick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrypickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherrypickApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pj/cherrypick/test/CherryPickControllerTest.java
@@ -0,0 +1,14 @@
+package com.pj.cherrypick.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // IoC : 스프링 컨테이너가 관리
+public class CherryPickControllerTest {
+
+	// http://localhost:8080/test/hello
+	@GetMapping("test/hello")
+	public String hello() {
+		return "<h1>Hello SpringBoot</h1>";
+	}
+}
