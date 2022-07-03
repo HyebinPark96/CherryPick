c8b2b99a312db81bed8a05e441984c35b05253fc
AtomicLiquors <gyqls234@gmail.com>
2022-07-03 23:37:48 +0900

    [0703/chb] demo frontend&setup

---
+++ src/main/java/com/pj/cherrypick/controller/SampleController.java
@@ -0,0 +1,63 @@
+package com.pj.cherrypick.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+
+	@GetMapping(value = "/demo1")
+	public void demo1() {
+		System.out.println("demo1 호출");
+	}
+	
+	@GetMapping(value = "/demo2")
+	public void demo2() {
+		System.out.println("demo2 호출");
+	}
+	
+	@GetMapping(value = "/demo3")
+	public void demo3() {
+		System.out.println("demo3 호출");
+	}
+	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
