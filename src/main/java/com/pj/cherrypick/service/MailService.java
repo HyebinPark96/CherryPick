6b1448aab20ffe209d3e66c14a31fa6a672d0a5d
seoha-dev <seoha.dev@gmail.com>
2022-07-09 04:12:34 +0900

    [0709/seoha] conflict on mailservice fixed

--- src/main/java/com/pj/cherrypick/service/MailService.java
+++ src/main/java/com/pj/cherrypick/service/MailService.java
@@ -1,50 +1,12 @@
 package com.pj.cherrypick.service;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
 
-=======
->>>>>>> 90c2a17dc164839599fa280dd04f36e59f98bd82
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
-
-@Service
-public class MailService {
-     
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    public void sendMail() {
-        
-        // 수신 대상을 담을 ArrayList 생성
-        ArrayList<String> toUserList = new ArrayList<>();
-        
-        // 수신 대상 추가
-        toUserList.add("수신대상1@gmail.com");
-        toUserList.add("수신대상2@naver.com");
-        
-        // 수신 대상 개수
-        int toUserSize = toUserList.size();
-        
-        // SimpleMailMessage (단순 텍스트 구성 메일 메시지 생성할 때 이용)
-        SimpleMailMessage simpleMessage = new SimpleMailMessage();
-        
-        // 수신자 설정
-        simpleMessage.setTo((String[]) toUserList.toArray(new String[toUserSize]));
-        
-        // 메일 제목
-        simpleMessage.setSubject("Subject Sample");
-        
-        // 메일 내용
-        simpleMessage.setText("Text Sample");
-        
-        // 메일 발송
-        javaMailSender.send(simpleMessage);
-    }
-=======
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -69,5 +31,5 @@ public class MailService {
 		
 		System.out.println("send");
 	}
->>>>>>> 90c2a17dc164839599fa280dd04f36e59f98bd82
+
 }
