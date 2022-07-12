3d818cec8ddade5a6e6244d049acfd5830a17424
HyebinPark96 <bini2718@naver.com>
2022-07-12 20:41:07 +0900

    [0712/hyebin] divide find id/pwd function between BizMember and member

--- src/main/java/com/pj/cherrypick/controller/BizMemberController.java
+++ src/main/java/com/pj/cherrypick/controller/BizMemberController.java
@@ -9,5 +9,21 @@ import com.pj.cherrypick.domain.BizMemberVO;
 
 @Controller
 public class BizMemberController {
-
+	
+	// http://localhost/auth/findBUsernameForm
+	@GetMapping("/auth/findBUsernameForm")
+	public String findBUsernameForm() {
+		return "bizMember/findusernameForm"; // /WEB-INF/views/templates/bizMember/findusernameForm
+	}
+	
+	// http://localhost/auth/findBPasswordForm
+	@GetMapping("/auth/findBPasswordForm")
+	public String findBPasswordForm() {
+		return "bizMember/findPasswordForm"; // /WEB-INF/views/templates/bizMember/findPasswordForm
+	}
+	
+	
+	
+	
+	
 }
