3bed63a855fdce7d94fd8255d5e12eac7e2b5f2d
AtomicLiquors <gyqls234@gmail.com>
2022-07-15 13:54:23 +0900

    [07/15] SampleController minor addition

--- src/main/java/com/pj/cherrypick/controller/SampleController.java
+++ src/main/java/com/pj/cherrypick/controller/SampleController.java
@@ -3,6 +3,7 @@ package com.pj.cherrypick.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.pj.cherrypick.domain.BizMemberVO;
@@ -16,35 +17,12 @@ public class SampleController {
 		return "demomain";
 	}
 	
-	@GetMapping(value = "/demo1")
-	public void demo1() {
-		
+	@PostMapping("/bizMember/storeRegister")
+	public String storeManagement(@SessionAttribute(name = "bizMember", required = false) BizMemberVO bizMember, Model model) {
+		model.addAttribute("bizMember", bizMember);
+		return "bizMember/storeRegister"; // model 들고 뷰로 이동
 	}
 	
-	@GetMapping(value = "/demo2")
-	public void demo2() {
-		
-	}
-	
-	@GetMapping(value = "/demo3")
-	public void demo3() {
-		
-	}
-	
-	
-	@GetMapping(value = "/demoreview")
-	public void demoreview() {
-	}
-	
-	@GetMapping(value = "/regcafe")
-	public String demoregcafe() {
-		return "/bizmember/regcafe";
-	}
-	
-	
-	@GetMapping(value = "/demoadmin")
-	public void demoadmin() {
-	}
 	
 	@GetMapping(value = "/test")
 	public String test(Model model) {
