c5e18dbab1e5eea1a93700a6ab3f95259c5c0337
AtomicLiquors <gyqls234@gmail.com>
2022-07-17 22:39:11 +0900

    [0717/chb] .

--- src/main/java/com/pj/cherrypick/CherrypickApplication.java
+++ src/main/java/com/pj/cherrypick/CherrypickApplication.java
@@ -4,14 +4,19 @@ import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import com.pj.cherrypick.service.StorageService;
+import com.pj.cherrypick.storage.StorageProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class CherrypickApplication {
 
 	public static void main(String[] args) {
@@ -28,6 +33,15 @@ public class CherrypickApplication {
 		
 		return sessionFactory.getObject();
 	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
 	
 	
 }
---
+++ src/main/java/com/pj/cherrypick/storage/StorageProperties.java
@@ -0,0 +1,21 @@
+package com.pj.cherrypick.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+	/**
+	 * Folder location for storing files
+	 */
+	private String location = "uploads/img";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
