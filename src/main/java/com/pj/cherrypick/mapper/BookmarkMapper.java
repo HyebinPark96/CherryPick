41e0625df0eb2ecc28de774753e91f16a740c411
seoha-dev <seoha.dev@gmail.com>
2022-07-20 17:49:17 +0900

    [0720/seoha] list bookmark updated

--- src/main/java/com/pj/cherrypick/mapper/BookmarkMapper.java
+++ src/main/java/com/pj/cherrypick/mapper/BookmarkMapper.java
@@ -16,7 +16,13 @@ public interface BookmarkMapper {
 	// 내(username)가 이 카페(cno)를 북마크했는지 체크
 	BookmarkVO checkCafeBmk(Map<String, Object> m) throws Exception;
 	
+	// 내(username)가 전체 카페(cafe/all) 북마크했는지 체크
 	BookmarkVO checkCafeBmkAll(BookmarkVO vo) throws Exception;
+	
+	
+	// 내(username)가 이 리스트(lino)를 북마크 했는지 체크
+	BookmarkVO checkListBmk(Map<String, Object> m) throws Exception;	
+	
 	
 	// 북마크 카페 추가
 	public int addBmkc(String username, int cno) throws Exception;
@@ -24,5 +30,9 @@ public interface BookmarkMapper {
 	// 북마크 카페 삭제
 	public int delBmkc(String username, int cno) throws Exception;
 	
-	public int chkBmkc(String username, int cno) throws Exception;
+	// 북마크 리스트 추가
+	public int addBmkli(String username, int lino) throws Exception;
+	
+	// 북마크 리스트 삭제
+	public int delBmkli(String username, int lino) throws Exception;
 }
--- src/main/resources/static/js/cafe.js
+++ src/main/resources/static/js/cafe.js
@@ -24,7 +24,7 @@
 
 // cno -> cafe/cno (카페개별페이지) 보내기
 function sendToInfo(variable) {
-	location.href = `/cafe/${variable}`;
+	location.href = `/cafe/info/${variable}`;
 }
 
 // lino -> cafe/list/{lino} (리스트 개별페이지) 보내기
@@ -33,28 +33,22 @@ function sendToListInfo(variable) {
 }
 
 
-// 북마크 여부 확인
+// 카페 북마크 여부 확인
 $(document).ready(function() {
 	
-	var bmkList = new Array();
-	var bmkList = document.getElementsByName("bmk-list");
-
-	for (var i = 0; i < bmkList.length; i++) {
-		console.log(bmkList[i].value);
-	}
 	
-	
-	//chk 1=있음 0=없음
-	let chk = document.getElementById('bmk_check').value	
+//chk 1=있음 0=없음
+	let chk = document.getElementById('bmkli_check').value	
 	const cno = $("#cno").val();
 	const username = $("#username").val();
 	console.log("cno:"+cno);
-	console.log("username:"+username);
+//	console.log("username:"+username);
 	const bmkImg = document.getElementById("bmkImg")
 	console.log("chk:"+chk);
 
-	if (chk > 0) {
+	if (chk = 1 ) {
 		bmkImg.src = "/img/bmk.png";
+		
 	} else {
 		bmkImg.src = "/img/bmk-empty.png";
 	}
@@ -85,7 +79,50 @@ $(document).ready(function() {
 });
 
 
+// 북마크 여부 확인 (리스트)
+$(document).ready(function() {
+	
+	
+//chk 1=있음 0=없음
+	let chk = document.getElementById('bmkli_check').value
+	const lino = $("#lino").val();
+	const username = $("#username").val();
+	console.log("lino:"+lino);
+	console.log("username:"+username);
+	const bmkImg = document.getElementById("bmkliImg")
+	console.log("chk:"+chk);
 
+	if (chk == 1 ) {
+		bmkImg.src = "/img/bmk.png";
+		
+	} else {
+		bmkImg.src = "/img/bmk-empty.png";
+	}
+
+	// 북마크 버튼 클릭시 실행되는 코드
+	$("#bmkliImg").on("click", function() {
+		$.ajax({
+			url: '/cafe/bmkli/',
+			type: 'GET',
+			data: { 'lino': lino, 'username': username },
+			success: function(data) {
+				console.log(data);
+				if (data == 1) {
+					$("#bmkliImg").attr("src", "/img/bmk.png");
+				} else {
+					$("#bmkliImg").attr("src", "/img/bmk-empty.png");
+				}
+			}, error: function() {
+				$("#bmkliImg").attr("src", "/img/bmk-empty.png");
+				console.log('오타')
+				
+			}
+
+		});
+
+	});
+	
+});
 
 
 
