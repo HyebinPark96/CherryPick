476a05631610c9ac6bb59b45f59fac0d2ee3cf23
HyebinPark96 <bini2718@naver.com>
2022-07-26 11:03:26 +0900

    [0726/hyebin] add cafe info my reveiw update/delete func

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -8,7 +8,10 @@ let review = {
 		$('#reviewUpdateBtn').on("click", () => {
 			this.reviewUpdate();
 		});
-	
+		
+		$('#reviewUpdateFromCafeBtn').on("click", () => {
+			this.reviewUpdateFromCafe();
+		});
 	
 	},
 
@@ -75,8 +78,45 @@ let review = {
 			alert(JSON.stringify(error));
 		});
 	},
+	
+	
+	
 
 
+
+
+	reviewUpdateFromCafe: function() {
+		
+		var waitingValue = parseInt($("#waiting").val());
+		
+		let data = {
+			rno: $("#rno").val(),
+			username: $("#username").val(),
+			title: $("#title").val(),
+			content: $('#content').summernote('code'), // 태그나 문자열 따질 것 없이 전부 가져온다.
+			score: $("#score").val(),
+			waiting: waitingValue,
+			cno: $("#cno").val()
+		};
+		
+		let page = {
+			num: $("#num").val()
+		}
+		
+		$.ajax({
+			type: "PUT",
+			url: "/review/updateReviewProc",
+			data: JSON.stringify(data), 
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("리뷰가 수정되었습니다.");
+			location.href="/cafe/info/"+ data.cno + "?num=" + page.num;
+		}).fail(function(error) {
+			// 실패한 경우 호출
+			alert(JSON.stringify(error));
+		});
+	},
 }
 
 review.init();
