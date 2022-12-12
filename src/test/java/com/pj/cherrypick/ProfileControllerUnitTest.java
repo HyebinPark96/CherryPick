aecb1e87a9bd2799a53e343fe349457effcf8631
seohakim <seoha-dev@gmail.com>
2022-12-12 21:42:45 +0900

    ProfileControllerUnitTest updated

--- src/test/java/com/pj/cherrypick/ProfileControllerUnitTest.java
+++ src/test/java/com/pj/cherrypick/ProfileControllerUnitTest.java
@@ -45,6 +45,40 @@ public class ProfileControllerUnitTest {
     }
 
     @Test
+    public void real_profile이_없으면_real1_조회된다() {
+        //given
+        String expectedProfile = "real1";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_real2_조회된다() {
+        //given
+        String expectedProfile = "real2";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
     public void active_profile이_없으면_default가_조회된다() {
         //given
         String expectedProfile = "default";
