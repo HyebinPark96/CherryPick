7115b6c971c5e5597ebd8c8df0760ed987cbb03c
seohakim <seoha-dev@gmail.com>
2022-12-20 19:11:14 +0900

    deploy.sh profile set

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml \
+    #-Dspring.config.location=classpath:/application.yml \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
