d4e7925b1767dbc6a3c83af3b0ec1a32539a9030
seohakim <seoha-dev@gmail.com>
2024-06-25 16:17:41 +0900

    random cafe data updated

--- src/main/java/com/pj/cherrypick/controller/CafeInfoController.java
+++ src/main/java/com/pj/cherrypick/controller/CafeInfoController.java
@@ -13,7 +13,37 @@ public class CafeInfoController {
 
     @GetMapping("/update-random-values")
     public String updateRandomValues() {
-        cafeInfoService.updateNullColumnWithRandomValue();
+        cafeInfoService.updateNullColumnsWithRandomValues();
+        return "Update completed";
+    }
+
+    @GetMapping("/import-cafe-logos")
+    public String importCafeLogos() {
+        cafeInfoService.updateNullCImageColumns();
+        return "Update completed";
+    }
+
+    @GetMapping("/import-cafe-seats")
+    public String importCafeSeats() {
+        cafeInfoService.updateNullSeatsColumns();
+        return "Update completed";
+    }
+
+    @GetMapping("/import-cafe-time")
+    public String importCafeTime() {
+        cafeInfoService.updateTimes();
+        return "Update completed";
+    }
+
+    @GetMapping("/import-cafe-bid")
+    public String importCafeBid() {
+        cafeInfoService.updateNullBid();
+        return "Update completed";
+    }
+
+    @GetMapping("/import-cafe-tags")
+    public String importCafeTags() {
+        cafeInfoService.updateNullCtag();
         return "Update completed";
     }
 
